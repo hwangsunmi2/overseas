4441618d0307a8f43caad57e7ed90c33560a8d73
red <red@wordncode.com>
2022-10-19 18:09:31 +0900

    [red] 221019 메인 작업 중

---
+++ resource/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    mainSwiper();
+    mainTab();
+
+});
+function mainSwiper() {
+    var mainswiper = new Swiper(".section1.mySwiper", {
+        pagination: {
+          el: ".section1 .swiper-pagination",
+        },
+      }); 
+}
+
+function mainTab() {
+    var $notice = $('.tab_wrap');
+    var $noticeTab = $notice.find('h3');
+    var $noticeCont = $notice.find('.tab_cont');
+
+    $noticeTab.on('click focusin', function(e) {  
+        e.preventDefault();
+        var tabID = this.id.split("_")[1];
+        console.log('tabID',tabID);
+        var $nListID = $('#list_' + tabID);
+        if ($nListID.css("display") == "none") {
+            $noticeTab.removeClass('on');
+            $noticeCont.css('display','none');
+        }
+        $(this).addClass("on");
+        $nListID.css('display','block');
+    });
+}
